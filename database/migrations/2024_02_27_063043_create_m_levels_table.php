dd8d2baa427d5fd4c492822a957712e0a8ae8c07
iqballmhtrm <iqbalmuctar@gmail.com>
2024-02-27 13:44:30 +0700

    Laporkan hasil Praktikum-2.1

--- database/migrations/2024_02_27_063043_create_m_levels_table.php
+++ database/migrations/2024_02_27_063043_create_m_levels_table.php
@@ -11,19 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('<m_level>', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_kode',10);
-            $table->string('level_nama',100);
+        Schema::create('m_levels', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
 
-    /**p
+    /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('<m_level>');
+        Schema::dropIfExists('m_levels');
     }
 };
