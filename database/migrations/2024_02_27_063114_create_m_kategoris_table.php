de49bea8c4d6b7968c84fd43c3857606601f6b30
iqballmhtrm <iqbalmuctar@gmail.com>
2024-02-27 13:54:49 +0700

    Praktikum 2.2

--- database/migrations/2024_02_27_063114_create_m_kategoris_table.php
+++ database/migrations/2024_02_27_063114_create_m_kategoris_table.php
@@ -11,17 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_kategoris', function (Blueprint $table) {
-            $table->id();
+        Schema::create('m_level', function (Blueprint $table) {
+            $table->id('level_id');
+            $table->string('level_kode',10);
+            $table->string('level_nama',100);
             $table->timestamps();
         });
     }
 
-    /**
+    /**p
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_kategoris');
+        Schema::dropIfExists('m_level');
     }
 };
