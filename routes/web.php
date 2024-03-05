4890bc650d690de3a4275b22b38ef5dd5ebc5e84
iqballmhtrm <iqbalmuctar@gmail.com>
2024-03-05 21:04:23 +0700

    Praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-
 Route::get('/kategori', [KategoriController::class, 'index']);
-
 Route::get('/user', [UserController::class, 'index']);
