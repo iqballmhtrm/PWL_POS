d7076c9ffdbc15704a1fc5bc5fdf979ef8b95971
iqballmhtrm <iqbalmuctar@gmail.com>
2024-03-13 00:03:27 +0700

    Praktikum-1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -17,8 +17,5 @@ class UserModel extends Model
     // protected $fillable = ['level_id','username','nama','password'];
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+
 }
