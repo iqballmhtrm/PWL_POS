a52d12044d1ed02797e242f997153ce566e83a2b
iqballmhtrm <iqbalmuctar@gmail.com>
2024-03-27 02:12:05 +0700

    jobshet 5 10

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\KategoriModel;
+//jobshet 5 
+//10
 use Illuminate\Http\Request;
 use App\DataTables\KategoriDataTable;
 
@@ -13,46 +13,46 @@ class KategoriController extends Controller
         return $dataTable->render('kategori.index');
     }
 
-    public function create()
-    {
-        return view('kategori.create');
-    }
+    // public function create()
+    // {
+    //     return view('kategori.create');
+    // }
     
-    public function store(Request $request)
-    {
-        KategoriModel::create([
-            'kategori_code' => $request->codekategori,
-            'kategori_nama' => $request->namakategori,
-        ]);
+    // public function store(Request $request)
+    // {
+    //     KategoriModel::create([
+    //         'kategori_code' => $request->codekategori,
+    //         'kategori_nama' => $request->namakategori,
+    //     ]);
         
-        return redirect('/kategori');
-    }
+    //     return redirect('/kategori');
+    // }
 
-    public function edit($id)
-    {
-        $kategori = KategoriModel::find($id);
-        return view('kategori.edit', compact('kategori'));
-    }
+    // public function edit($id)
+    // {
+    //     $kategori = KategoriModel::find($id);
+    //     return view('kategori.edit', compact('kategori'));
+    // }
 
-    public function update($id, Request $request)
-    {
-        $kategori = KategoriModel::find($id);
-        if (!$kategori) {
-            return redirect()->back()->with('error', 'Kategori tidak ditemukan.');
-        }
+    // public function update($id, Request $request)
+    // {
+    //     $kategori = KategoriModel::find($id);
+    //     if (!$kategori) {
+    //         return redirect()->back()->with('error', 'Kategori tidak ditemukan.');
+    //     }
 
-        $kategori->kategori_code = $request->codeKategori;
-        $kategori->kategori_nama = $request->namaKategori;
-        $kategori->save();
+    //     $kategori->kategori_code = $request->codeKategori;
+    //     $kategori->kategori_nama = $request->namaKategori;
+    //     $kategori->save();
 
-        return redirect('/kategori')->with('success', 'Kategori berhasil diperbarui.');
-    }
+    //     return redirect('/kategori')->with('success', 'Kategori berhasil diperbarui.');
+    // }
 
 
-    public function destroy($id)
-    {
-        KategoriModel::destroy($id);
-        return redirect('/kategori');
-    }
+    // public function destroy($id)
+    // {
+    //     KategoriModel::destroy($id);
+    //     return redirect('/kategori');
+    // }
 }
 
